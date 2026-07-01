0a2129818a59ba00f20d7d2e786d0ea5642d3fd2
junaiddata <junaiddataanalyst@gmail.com>
2026-07-01 12:31:53 +0400

    AAA

--- hr_module/settings.py
+++ hr_module/settings.py
@@ -140,6 +140,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
